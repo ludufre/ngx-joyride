859633ce91e36e63bc248ae6710be90312d2011a
Luan Freitas <eu@luanfreitas.com>
2023-01-01 18:00:24 -0300

    fix iOS

--- projects/ngx-joyride/src/lib/services/document.service.ts
+++ projects/ngx-joyride/src/lib/services/document.service.ts
@@ -41,7 +41,7 @@ export class DocumentService implements IDocumentService {
     }
 
     getElementFixedTop(elementRef: ElementRef) {
-        return elementRef.nativeElement.getBoundingClientRect().top;
+        return elementRef.nativeElement.getElementAbsoluteTop().top;
     }
 
     getElementFixedLeft(elementRef: ElementRef) {
